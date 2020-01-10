102b6fd7774eb623c8db706f491ff47436705341
Cleveland Cooperative <cleveland.cooperative@gmail.com>
2020-01-10 13:25:08 -0500

    update to User Controller and Views files

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,15 @@
 class Api::UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
   def create
     user = User.new(
       name: params[:name],
@@ -15,4 +25,25 @@ class Api::UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(
+    name: params[:name], 
+    email: params[:email],
+    phone: params[:phone],
+    high_school: params[:high_school]
+    )
+    if @user.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @user.errors.full_message}, status: unprocessable_entity
+    end
+  end
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
 
+    get "/users" => "users#index"
+    get "/users" => "users#show"
     post "/users" => "users#create"
+    patch "/users" => "users#update"
 
     post "/sessions" => "sessions#create"
   end
